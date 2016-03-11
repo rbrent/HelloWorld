81015163b5c12a2925177efb2d702a6874454817
Rob Brent <rbrent@outlook.com>
2016-03-11 15:46:51 -0500

    yeah

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World using C#!");
+            Console.WriteLine("Seriously.... is this thing on?");
         }
     }
 }
